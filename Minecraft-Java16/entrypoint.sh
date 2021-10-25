978d0c0a86ef14ea7451b4efeec9b97ee5251199
ILoveCatto <192010300107@umsida.ac.id>
2021-10-25 12:16:32 +0700

    Initial commit

---
+++ Minecraft-Java16/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Shows java version
+java -version
+
+# Start the minecraft server (Bring your own server.jar)
+java -Xms128M -Xmx${SERVER_MEMORY} -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=15 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar server.jar nogui
