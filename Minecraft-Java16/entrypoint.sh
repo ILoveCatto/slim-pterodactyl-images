a2d16fc48c76d52e1aee39428d867d4ccd6658b3
ILoveCatto <192010300107@umsida.ac.id>
2021-10-26 06:33:21 +0700

    Rewriting the sccript

--- Minecraft-Java16/entrypoint.sh
+++ Minecraft-Java16/entrypoint.sh
@@ -4,6 +4,9 @@ cd /home/container
 # Output Current Java Version
 java -version ## only really needed to show what version is being used. Should be changed for different applications
 
+# Download latest minecraft server version / bring your own server.jar
+curl -sLo /home/container/server.jar https://serverjars.com/api/fetchJar/purpur/latest
+
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
